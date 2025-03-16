9dad78c6c833569c3d261ad6d8b1b202237c3251
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-03-17 02:09:30 +0600

    milestone.name

--- js/main.js
+++ js/main.js
@@ -7,7 +7,25 @@ function loadMilestones (){
 
 
 
-    milestones.innerHTML = milestonesData ;
+    milestones.innerHTML = `${milestonesData.map(function(milestone){
+        return `<div class="milestone border-b">
+            <div class="flex">
+              <div class="checkbox"><input type="checkbox" /></div>
+              <div>
+                <p>
+                  ${milestone.name}
+                  <span><i class="fas fa-chevron-down"></i></span>
+                </p>
+              </div>
+            </div>
+            <div class="hidden_panel">
+              <div class="module border-b">
+                <p>Module Name</p>
+              </div>
+            </div>
+          </div>`;
+
+    }).join(" ")}`;
 
 
     
