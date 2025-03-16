7d8e4293cbf853802d59eb7cea03378c0cae6203
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-03-17 01:58:11 +0600

    hack

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const milestonesData = JSON.parse(data).data;
+
+
+//load course milestones data 
+function loadMilestones (){
+    const milestones = document.querySelector('.milestones');
+
+
+
+    milestones.innerHTML = milestonesData ;
+
+
+    
+}
+loadMilestones ()
